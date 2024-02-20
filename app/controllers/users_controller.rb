66798c3520e758a5cf8e8f12740a194d71a0af7b
rustimi <michele.rusticucci@gmail.com>
2024-02-20 23:51:37 +0100

    Signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,20 @@
 class UsersController < ApplicationController
   before_action :set_user, only: :show
   def index
+    render json: {users: User.all}
   end
 
   def show
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      # Respond with appropriate message or token
+      render json: { status: 'User created successfully', user_id: user.id}, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
   end
 
   def update
@@ -18,5 +26,8 @@ class UsersController < ApplicationController
   private
     def set_user
       @user = User.find(params[:id])
+    end
+    def user_params
+      params.permit(:fname, :lname, :email, :password, :password_confirmation)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_14_181428) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_20_223501) do
   create_table "users", force: :cascade do |t|
     t.string "fname"
     t.string "lname"
     t.string "email"
-    t.binary "document"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
-    t.string "password_confirmation"
   end
 
 end
