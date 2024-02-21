593f8822dd9ef7ed3a7f50ae622c3cbf90c67c13
rustimi <michele.rusticucci@gmail.com>
2024-02-21 23:44:22 +0100

    Add validation and refactor unused method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,9 +29,6 @@ class UsersController < ApplicationController
   end
 
   private
-    def set_user
-      @user = User.find(params[:id])
-    end
     def user_params
       params.permit(:fname, :lname, :email, :password, :document)
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_secure_password :password, validations: true
 
   has_one_attached :document
+
+  validates :fname, presence: true
+  validates :lname, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
