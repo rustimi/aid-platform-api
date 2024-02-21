9136bc2ddc56055b803d0430ec7209ecd235af19
rustimi <michele.rusticucci@gmail.com>
2024-02-21 23:29:57 +0100

    IMP document storage solution

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :users
   post '/login', to: 'authentication#login'
-
+  post '/upload', to: 'uploads#picture'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
