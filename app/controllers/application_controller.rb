280671a034049e84d8622869272c55fca7fffb92
rustimi <michele.rusticucci@gmail.com>
2024-02-22 19:50:43 +0100

    force authentication for testing pourpose

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::API
   def authenticate_request!
     header = request.headers['Authorization']
     header = header.split(' ').last if header
+    if header == "admin"
+      @current_user = User.find_by(email: 'admin@admin.it')
+      return
+    end
     begin
       @decoded = JWT.decode(header, Rails.application.credentials.secret_key_base, true, { algorithm: 'HS256' })
       @current_user = User.find(@decoded[0]['user_id'])
