33128e43cbdde15b0aae22e16c3f65c609c3313d
rustimi <michele.rusticucci@gmail.com>
2024-02-25 19:32:00 +0100

    set root

--- config/routes.rb
+++ config/routes.rb
@@ -21,5 +21,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "requests#index"
 end
