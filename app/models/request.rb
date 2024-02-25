c7c10badba06f8d0c1db25ff144706f85aeca3ef
rustimi <michele.rusticucci@gmail.com>
2024-02-25 17:31:13 +0100

    add validation to request fields

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,10 +1,26 @@
 class Request < ApplicationRecord
+  after_save :check_fulfillment_count
+
+
+  # related fields
   belongs_to :requester, class_name: 'User'
   has_many :volunteering_instances
   has_many :volunteers, through: :volunteering_instances, source: :user
 
 
+  # validation
   validates :description, :type, :status, :latitude, :longitude, presence: true
+  validates :type, inclusion: { in: ['One time task', 'Material need'],
+                                message: "%{value} is not a valid type" }
 
+  validates :status, inclusion: { in: ['unpublished', 'fulfilled', 'active'],
+                                message: "%{value} is not a valid type" }
 
+  private
+  def check_fulfillment_count
+    if self.fulfillment_count == 5
+      # using update_column, skips validation and avoids triggering this function again
+      self.update_column(:status, 'unpublished')
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,12 @@
 class User < ApplicationRecord
   # validation
-  validates :fname, presence: true
-  validates :lname, presence: true
+  validates :fname, :lname, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   has_secure_password :password, validations: true
 
   # related fields
   has_one_attached :document
   has_many :requests, foreign_key: 'requester_id'
-
   # As a volunteer
   has_many :volunteering_instances
   has_many :volunteered_requests, through: :volunteering_instances, source: :request
