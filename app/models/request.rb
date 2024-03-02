f0d0d3a0c4380d67ab396e7aebb217fd56cd1f50
rustimi <michele.rusticucci@gmail.com>
2024-03-02 19:16:58 +0100

    IMP geokit-rails

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,7 @@
 class Request < ApplicationRecord
+  acts_as_mappable :default_units => :kms,
+                   :lat_column_name => :latitude,
+                   :lng_column_name => :longitude
 
   # related fields
   belongs_to :requester, class_name: 'User'
