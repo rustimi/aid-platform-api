b0604b3ade3c0efdb67dbcef9631df65e6d92f75
rustimi <michele.rusticucci@gmail.com>
2024-03-11 23:18:52 +0100

    FIX conversations endpoint should return members name

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -6,13 +6,31 @@ class ConversationsController < ApplicationController
   def index
     if @current_user == @request.requester
       # The current user is the requester, so show all conversations for the request
-      render json: @request.conversations, status: :ok
+      conversations = @request.conversations
     else
       # The current user is not the requester, so show only conversations involving the current user for this request
       # Since the conversation is always initialized by the volunteer, it's always the sender
-      conversation = @request.conversations.where(sender: @current_user)
-      render json: conversation, status: :ok
+      conversations = @request.conversations.where(sender: @current_user)
     end
+    if conversations.empty?
+      conversations_with_names = []
+    else
+      conversations_with_names = conversations.map do |conversation|
+        {
+          id: conversation.id,
+          sender: {
+            fname: conversation.sender.fname,
+            lname: conversation.sender.lname
+          },
+          receiver: {
+            fname: conversation.receiver.fname,
+            lname: conversation.receiver.lname
+          }
+        }
+      end
+    end
+
+    render json: conversations_with_names, status: :ok
   end
 
 
@@ -20,7 +38,7 @@ class ConversationsController < ApplicationController
   private
   def check_authorization
     # Must be the requester or a volunteer
-    if @current_user != @request.requester and !@request.volunteers.exists?(@current_user.id)
+    if @current_user != @request.requester and !@request.volunteers.exists?(id: @current_user.id)
       render json: { error: "Not authorized see the conversations!" }, status: :forbidden
     end
   end
